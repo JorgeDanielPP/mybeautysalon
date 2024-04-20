a38dcc10bbad96dc0a61621bf98af65946d80309
JorgeDanielPP <jorgedpolancop@gmail.com>
2024-04-20 16:44:15 -0400

    Se crea capas y se configura el DbContext con Scaffold-DbContext porque el Entity Framework me estaba ocasionando error.

---
+++ MyBeautySalon.Persistence/Appointments/Appointment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyBeautySalon.Persistence.Appointments;
+
+public partial class Appointment
+{
+    public int? Id { get; set; }
+
+    public DateTime? Date { get; set; }
+
+    public string? CustomerName { get; set; }
+}
---
+++ MyBeautySalon.Persistence/Appointments/AppointmentsDbContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyBeautySalon.Persistence.Appointments;
+
+public partial class AppointmentsDbContext : DbContext
+{
+    public AppointmentsDbContext()
+    {
+    }
+
+    public AppointmentsDbContext(DbContextOptions<AppointmentsDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Appointment> Appointments { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-V6BJ4DJ\\SQLEXPRESS;Database=AppointmentsDb;Integrated Security=True;Encrypt=False;Trusted_Connection=True;", x => x.UseNetTopologySuite());
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Appointment>(entity =>
+        {
+            entity.HasNoKey();
+
+            entity.Property(e => e.CustomerName)
+                .HasMaxLength(150)
+                .IsUnicode(false);
+            entity.Property(e => e.Date).HasColumnType("datetime");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
