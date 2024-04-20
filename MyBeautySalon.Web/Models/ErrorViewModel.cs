6f46944d89a996bb04509bc0748c19ea26f04325
JorgeDanielPP <jorgedpolancop@gmail.com>
2024-04-20 10:49:49 -0400

    creando el proyecto

---
+++ MyBeautySalon.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyBeautySalon.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
