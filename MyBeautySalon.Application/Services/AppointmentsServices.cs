9d964c2497eaa5590b8a921014bc779123738aba
JorgeDanielPP <jorgedpolancop@gmail.com>
2024-04-20 22:45:01 -0400

    creando la capa de servicio

---
+++ MyBeautySalon.Application/Services/AppointmentsServices.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyBeautySalon.Domain;
+using MyBeautySalon.Models;
+using MyBeautySalon.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBeautySalon.Application.Services
+{
+    public class AppointmentsServices
+    {
+        private readonly DataContext _context;
+        public AppointmentsServices(DataContext context)
+        {
+            _context = context; 
+        }
+        public async Task<List<AppointmentModel>> GetAll() { 
+            var appointmentsDb = await _context.Appointments.ToListAsync();
+            var appointments = new List<AppointmentModel>();
+            foreach (var appointmentsDb in appointmentsDb)
+            {
+                appointments.Add(new AppointmentModel()
+                {
+                    Id = appointmentsDb.Id,
+                    Date = appointmentsDb.Date,
+                    CustomerName = appointmentsDb.CustomerName
+                });
+
+                return appointments;
+
+            }
+   
+        
+        }
+        public async Task<AppointmentModel> Get(int Id)
+        {
+            var appointmentsDb = await _context.Appointments.FindAsync(Id);
+            var appointment = new AppointmentModel
+            {
+                Id = appointmentsDb.Id,
+                Date = appointmentsDb.Date,
+                CustomerName = appointmentsDb.CustomerName
+            };
+            return appointment;
+
+        }
+        public bool Create(AppointmentModel appointment)
+        {
+            try
+            {
+                _context.Appointments.Add(appointment);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+               
+            }
+            return false;
+
+        }
+        public bool Modified(AppointmentModel appointment)
+        {
+            try
+            {
+                _context.Appointments.Update(appointment);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+            }
+            return false;
+
+        }
+        public bool Remove(AppointmentModel appointment)
+        {
+            try
+            {
+                _context.Appointments.Remove(appointment);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+            }
+            return false;
+
+        }
+    }
+}
---
+++ MyBeautySalon.Web/Controllers/AppointmentsController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyBeautySalon.Application.Services;
+using MyBeautySalon.Domain;
+using MyBeautySalon.Models;
+using MyBeautySalon.Persistence;
+
+namespace MyBeautySalon.Web.Controllers
+{
+    public class AppointmentsController : Controller
+    {
+        private readonly AppointmentsServices _appointmentservices;
+        public AppointmentsController(AppointmentsServices appointmentsServices)
+        {
+            _appointmentservices = appointmentsServices;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+
+            var appointmentdb = _appointmentservices.GetAll();
+            return View(appointmentdb);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(AppointmentModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+           var succeded = _appointmentservices.Create(model);
+            if (succeded)
+                return RedirectToAction(nameof(Index));
+            else
+                ViewBag.Error = "Error Saving";
+                return View(model);
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> Edit(int cid)
+        {
+            // var appointmentdb =  _context.Appointments.FirstOrDefault(x => x.Id == id);
+            // var appointmentdb = _context.Appointments.Find(cid);
+            var appointmentdb = _appointmentservices.Get(cid);
+            return View(appointmentdb);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(AppointmentModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            // _context.Appointments.Update(model);
+            // _context.SaveChanges();
+            // return RedirectToAction(nameof(Index));
+            var succeded = _appointmentservices.Modified(model);
+            if (succeded)
+                return RedirectToAction(nameof(Index));
+            else
+                ViewBag.Error = "Error Saving";
+                return View(model);
+
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var appointmentdb = _appointmentservices.Get(id);
+            return View(appointmentdb);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(AppointmentModel model)
+        {
+            //   _context.Appointments.Remove(model);
+            //   _context.SaveChanges();
+            //   return RedirectToAction(nameof(Index));
+            var succeded = _appointmentservices.Remove(model);
+            if (succeded)
+                return RedirectToAction(nameof(Index));
+            else
+                ViewBag.Error = "Error Saving";
+            return View(model);
+
+        }
+    }
+}
